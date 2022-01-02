37b5ac018f7491ac171585b43cf7c35ef256a4fe
LYS <t108360147@ntut.org.tw>
2022-01-02 14:54:13 +0800

    hand input receipt sqlite version 1.0

---
+++ Receipt_test/app/src/main/java/com/example/receipt_test/myDBHelper.java
@@ -0,0 +1,34 @@
+package com.example.receipt_test;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class myDBHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "MY_RECEIPT";
+    private static final int DB_VERSION = 1;
+
+    public myDBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // 建立資料庫
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + DB_NAME + "(" +
+                "Receipt_Year integer NOT NULL, " +
+                "Receipt_Month text NOT NULL, " +
+                "Receipt_Day text NOT NULL, " +
+                "Receipt_Number text NOT NULL PRIMARY KEY)"
+                );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + DB_NAME);
+        onCreate(db);
+    }
+}
